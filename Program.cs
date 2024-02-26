b9b948c2ff694549c3356e19473a9b218357fdde
Sidswipe <sidswipexyz@gmail.com>
2024-02-26 18:56:18 +0100

    Logic for string to int conversion

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string? readResult; //initialize a nullable string to collect user input
+string stringEntd = ""; //initialze a string variable to store the user input 
+int stringtoNum = 0; //initialze an integer variable to hold the converted string
+bool validEntry = false; //a boolean to validate each iteration
+Console.WriteLine("Please enter a value between 5 and 10");
+
+do
+{
+    readResult = Console.ReadLine(); //waits and collects user input
+    if (readResult != null)  //as long as the user input is no longer void continue
+     {stringEntd = readResult;} //store the user input in the string variable
+
+    validEntry = int.TryParse(stringEntd, out stringtoNum);  //A method that coverts the string stored in stringEntd to numbers and stores it in stringtoNum
+
+if (validEntry == true) //if the conversion succeeds
+  {
+    if (stringtoNum < 5 || stringtoNum > 10) //conditional to make sure its between 5 and 10
+    {
+        validEntry = false;
+    Console.WriteLine($"You entered {stringtoNum} Please enter a value between 5 and 10");
+    }
+  }
+    else
+     {
+        Console.WriteLine("You entered a wrong input");
+        }
+
+} while(validEntry == false);
+
+ Console.WriteLine($"You entered {stringtoNum}, so your input is accepted");
+
---
+++ README.md
@@ -0,0 +1,10 @@
+# Logic that validates integer input
+
+Before the iteration block: The solution uses a Console.WriteLine() statement to prompt the user for an integer value between 5 and 10.<br>
+Inside the iteration block:<br>
+The logic uses a nullable string type and a Console.ReadLine() statement to obtain input from the user.<br>
+The Logic the int.TryParse(a, out b) Method to convert string to integer <br>
+If the integer value isn't between 5 and 10, A Console.WriteLine() statement is used to prompt the user for an integer value between 5 and 10.<br>
+The Logic ensures that the integer value is between 5 and 10 before exiting the iteration.
+
+## int.TryParse(a, out b)
